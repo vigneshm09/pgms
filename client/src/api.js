dfd77deb194779ee32a39235519e3ca20f56d62c
Vignesh <vignesh09102004@gmail.com>
2026-04-21 01:51:00 +0530

    fix api url

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { clearStoredSession, getAccessToken } from "./utils/session";
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://pgms-jzfk.onrender.com";
+export const API_BASE_URL = "https://pgms-jzfk.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL
